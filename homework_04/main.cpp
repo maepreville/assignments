605e4549b36812a559f223c84c867e720611870b
maepreville <54781760+maepreville@users.noreply.github.com>
2019-12-12 17:58:18 -0500

    Update main.cpp

--- homework_04/main.cpp
+++ homework_04/main.cpp
@@ -1,1 +1,45 @@
+/*Mae-Jeanne Preville
+ Date: Oct 15th 2019
+ Homework #3 - Dr. Griffin
+ Description: List based Queue that holds an animal
+ */
 
+#include <iostream>
+#include <fstream>
+#include "ListQueue.hpp"
+
+using namespace std;
+
+int main() {
+
+	ifstream fin("animal.txt");  // input file of animal info 
+
+	Animal *a;
+	string name;
+	double weight;
+	double scary;
+
+	ListQueue LQ;  // Queue instance 
+	
+	while (!fin.eof()) {
+		fin >> name >> weight >> scary;
+		a = new Animal(name, weight, scary); // allocate memory for an animal
+		LQ.Push(a);  // push animal onto the stack
+	}
+	
+	LQ.Print();
+	cout << "_____________" << endl;
+
+	
+	LQ.Pop();
+	LQ.Print();
+	cout << "_____________" << endl;
+	
+
+	LQ.Pop();
+	LQ.Print();
+	cout << "_____________" << endl;
+	
+	system("pause");
+	return 0;
+}
